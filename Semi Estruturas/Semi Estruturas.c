78b80ad2eff1b27a74e9db7a07697d96b241a0f2
unknown <luciel.vitor.bs@gmail.com>
2019-05-21 01:17:17 -0300

    Commit Semipresencial sobre Estrutura (struct)

--- Semi Estruturas/Semi Estruturas.c
+++ Semi Estruturas/Semi Estruturas.c
@@ -56,14 +56,14 @@ main(){
 	fflush(stdin);
 
 	
-	printf("\n Dseja procurar carro por placa (s/n)? ");
+	printf("\n Deseja procurar carro por placa (s/n)? ");
 	scanf("%c", &resp);
 	
 	fflush(stdin);
 	
 	while(resp != 's' && resp != 'n'){
 		printf("\n INSIRA UMA RESPOSTA VALIDA! --> (s/n)");
-		printf("\n Dseja procurar carro por placa (s/n)? ");
+		printf("\n Deseja procurar carro por placa (s/n)? ");
 		scanf("%c", &resp);
 		
 		fflush(stdin);
@@ -106,7 +106,7 @@ main(){
 			
 			while(resp != 's' && resp != 'n'){
 				printf("\n INSIRA UMA RESPOSTA VALIDA! --> (s/n)");
-				printf("\n Dseja procurar carro por placa (s/n)? ");
+				printf("\n Dseja procurar outro carro por placa (s/n)? ");
 				scanf("%c", &resp);
 				
 				fflush(stdin);
